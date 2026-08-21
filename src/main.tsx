357be5d471e4a90e377ef6dfcea0800a882ca12d
John Schloss <110794264+DatMack@users.noreply.github.com>
2026-08-21 04:55:06 -0400

    Show reader level progress in Shelfie

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { ReaderProgressCard } from './components/ReaderProgressCard'
 import './styles.css'
 import './future.css'
+import './readerProgress.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ReaderProgressCard />
   </React.StrictMode>,
 )
