03c31218c24a36f226a3a8ffc6e7a1f7886c6fd2
John Schloss <110794264+DatMack@users.noreply.github.com>
2026-08-21 08:18:22 -0400

    Enable clean shelf hover cards

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AuthGate } from './components/AuthGate'
 import './lib/demoDataCleanup'
+import './lib/shelfHoverCard'
 import './styles.css'
 import './future.css'
 import './readerProgress.css'
@@ -19,6 +20,7 @@ import './organizeShelf.css'
 import './shelfCleanup.css'
 import './shelfProfiles.css'
 import './realBookCovers.css'
+import './shelfHoverCard.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
