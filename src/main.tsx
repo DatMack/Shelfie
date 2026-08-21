8867f3ddd138145e9849825cecfa39826fb0ee7e
John Schloss <110794264+DatMack@users.noreply.github.com>
2026-08-21 08:49:05 -0400

    Reset stale shelf layouts and add geometry debug styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AuthGate } from './components/AuthGate'
 import './lib/demoDataCleanup'
+import './lib/shelfLayoutGeometryReset'
 import './lib/shelfHoverCard'
 import './styles.css'
 import './future.css'
@@ -22,6 +23,7 @@ import './shelfProfiles.css'
 import './realBookCovers.css'
 import './shelfHoverCard.css'
 import './shelfGeometryFix.css'
+import './shelfApiGeometryDebug.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
